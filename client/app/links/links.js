4fc1678587230b1d05f3c898b8685b436ff9c889
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-07 09:42:44 -0800

    fixed link getter function, link opener still failing

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,17 +1,29 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, $window, Links) {
   // Your code here
-  $scope.data = {};
+  $scope.links = {};
   $scope.getLinks = function() {
     Links.getAllLinks()
       .then(function (links) {
-        $scope.data.links = links;
+        $scope.links = links;
+        console.log(links);
       })
       .catch(function (err) {
         console.error(err);
       });
     // TODO
   };
+
+  $scope.openLink = function (code) {
+    Links.goToLink(code)
+      .then( function (result) {
+        console.log('Success');
+        $window.open(result.url);
+      })
+      .catch( function (err) {
+        console.error(err);
+      })
+  }
   $scope.getLinks();
 });
