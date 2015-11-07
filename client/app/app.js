370d9614c3824f40cde7fd7e5049d1e998fad94b
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2015-11-07 09:43:16 -0800

    minor updates

--- client/app/app.js
+++ client/app/app.js
@@ -22,7 +22,7 @@ angular.module('shortly', [
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
-    });
+    })
     // Your code here
     // controller for links
     // We add our $httpInterceptor into the array
